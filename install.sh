051d4afb23a2e77d0b7197a96e84a7d656575571
Jason Frantz <jasondfrantz@gmail.com>
2013-07-27 17:09:56 -0700

    Add install script, jacked from @sontek.

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+function link_one() {
+    src="${PWD}/$1"
+    tgt="${HOME}/${1/_/.}"
+
+    if [ -e "${tgt}" ] && [ ! -L "${tgt}" ]; then
+        echo "Backing up $tgt to $tgt.df.bak"
+        mv $tgt $tgt.df.bak
+    fi
+
+    ln -sf $src $tgt
+}
+
+for i in _*; do
+    link_one $i
+done
